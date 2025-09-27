eac3227d7c01c8ad4953264afd1d5fb3a221cff4
Alex Peirano <alexpeirano29@gmail.com>
2025-09-27 17:56:18 +0200

    choose.py work

--- choose.py
+++ choose.py
@@ -1,10 +1,32 @@
 import tkinter as tk
 
-number_of_days = 0
+# fonction qui restreint l'input
+def valider(input):
+    n = ["2","3","4","5","6"]
+    if input == ""  or input in n:
+        return True
+    return False 
+# fonction qui récupère l'input
+def afficher():
+    input = text_box.get()
+    print(input)
 
-window = tk.Tk()
-window.title("Choisissez le nombre de jours par semaine")
+def run_choose() -> None :
+    number_of_days = 0
+    global text_box
+    window = tk.Tk()
+    window.title("nombre de jours/semaine")
 
-T = tk.Text(window).pack()
+    window.geometry("300x300")
 
-tk.mainloop()
+    vcmd = (window.register(valider), "%P")
+
+    text_box = tk.Entry(window, validate="key", validatecommand=vcmd)
+    text_box.pack()
+
+    bouton = tk.Button(window, text="Valider", command=afficher).pack(pady=10)
+    
+
+    tk.mainloop()
+
+run_choose()
